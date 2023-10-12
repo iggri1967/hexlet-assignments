a4bacbe3412671ce988362ed092b236568b0bc48
Igor Grishkin <igri1967@gmail.com>
2023-10-12 20:42:08 +0300

    correction

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,30 +1,34 @@
-package exercise;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static java.lang.Character.toLowerCase;
-
 // BEGIN
-public class App{
-    public static void main(String[] args){
+public class App {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String letterSet = scanner.nextLine().toLowerCase();
         String word = scanner.nextLine().toLowerCase();
         System.out.println(scrabble(letterSet, word));
-        }
-    public static boolean scrabble(String letterSet, String word) {
-        ArrayList<Character> letterList = new ArrayList<>();
-        for (char letter : letterSet.toCharArray()) {
-        letterList.add(letter);
-        }
-        for (char letter : word.toCharArray()) {
-            if (!letterList.remove(toLowerCase(letter))) {
-            return false;
-            }
-        }
-        return true;
     }
 
+    public static boolean scrabble(String letterSet, String word) {
+        ArrayList<Character> letterList = new ArrayList<>();
+
+        for (char letter : letterSet.toCharArray()) {
+            letterList.add(letter);
+        }
+
+        for (char letter : word.toCharArray()) {
+
+            int index = letterList.indexOf(letter);
+            if (index != -1) {
+
+                letterList.remove(index);
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
-//END
+// END
